72504b3a0408e80ea587b1d611cbed7a280ff60f
Vladimir Fetisov <fetisov@sports.ru>
2019-05-26 04:35:28 +0300

    add ident flag parser

--- command/command.go
+++ command/command.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -11,6 +12,7 @@ type Command struct {
 	Args   []string
 	Paths  []string
 	Output string
+	Ident  string
 }
 
 type Options struct {
@@ -45,15 +47,17 @@ Options:
 		Version:          "v0.2.1",
 	}
 
+	c.parseFlags()
+
 	argsCount := len(c.Args)
 
 	// check the number of args
-	if argsCount <= 1 {
+	if argsCount == 0 {
 		// no arg -> print help msg
 		return fmt.Errorf(options.Help)
 	}
 
-	if argsCount == 2 {
+	if argsCount == 1 {
 		if strings.HasPrefix(c.Args[1], "-") {
 			switch c.Args[1] {
 			case "-v":
@@ -68,7 +72,7 @@ Options:
 		return fmt.Errorf(options.OutputFileNeeded)
 	}
 
-	c.Paths = c.Args[1 : argsCount-1]
+	c.Paths = c.Args
 	c.Output = c.Args[argsCount-1]
 
 	// check passed paths is existing.
@@ -81,4 +85,29 @@ Options:
 	}
 
 	return nil
+}
+
+func (c *Command) parseFlags() {
+	ident := flag.String("ident", "4s", `
+Flag "ident" defines the padding in the generated GraphQL scheme.
+
+It follows the next pattern: ident={n}{i},
+	* n - amount of idents
+	* i - ident ("t" for tabs and "s" for spaces)
+If "n" is not stated 1 will be used, 
+so "--ident=1t" is equal to "--ident=t"
+`)
+
+	flag.Parse()
+
+	// ident is never nil, so
+	// dereference without checking
+	c.Ident = *ident
+
+	// flag.Parse() remove program's name (aka os.Args[0])
+	// and parsed flags from os.Args, so
+	// flag.Args() = os.Args - os.Args[0] - parsed flags
+	// i.e flag.Args() contains only the paths to parse
+	// and the output filename.
+	c.Args = flag.Args()
 }
