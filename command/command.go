8103df9cea500851c65d93ac27ece14e468cd60a
Woonki Moon <woonki.moon@gmail.com>
2023-08-02 00:21:10 +0900

    v0.2.6

--- command/command.go
+++ command/command.go
@@ -32,7 +32,7 @@ func (c *Command) Check() error {
 		NotEnoughArgs:    "❌ Not enough arguments",
 		OutputFileNeeded: "❌ Output file argument is needed",
 		WrongOption:      "❌ Wrong options",
-		Version:          "v0.2.3",
+		Version:          "v0.2.6",
 	}
 
 	help := flag.Bool("h", false, "show the help")
